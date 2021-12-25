b9c59d64fb9e5bab27acbe4e298c4c5dd845ae55
Lightblues <Lightblues@users.noreply.github.com>
2021-12-25 04:20:50 +0000

    deploy: 3f16c529397d2e44824077694d53d37a2bc243e2

---
+++ js/baidu-tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?07cc68228b5c4f4b4d49b36c1d3656c0";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
